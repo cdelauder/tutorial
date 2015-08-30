61b274d0b847d705aa428e7ca26e18e58dc0bba5
cdelauder <cdelauder@gmail.com>
2015-08-30 12:29:09 -0700

    add the next step which is a celsius/farhenheit converter

--- hello.c
+++ hello.c
@@ -3,6 +3,17 @@
 int main()
 {
   printf("hello world\n");
+  int fahr, celsius;
+  int lower, step, upper;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
   return 0;
 }
 
