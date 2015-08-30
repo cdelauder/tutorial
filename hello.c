9909de6a6c4ebadea3d224a383de957a2d872909
cdelauder <cdelauder@gmail.com>
2015-08-30 12:57:03 -0700

    now usiung floats instead oif ints

--- hello.c
+++ hello.c
@@ -3,15 +3,15 @@
 int main()
 {
   printf("hello world\n");
-  int fahr, celsius;
-  int lower, step, upper;
+  float fahr, celsius;
+  float lower, step, upper;
   lower = 0;
   upper = 300;
   step = 20;
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
   }
   return 0;
