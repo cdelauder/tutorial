98ed457e0ef377f1d4b9f43afe3835a0e6ca00ed
cdelauder <cdelauder@gmail.com>
2015-08-31 20:29:17 -0700

    completed 1.4 and it goes btoh ways now

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-  printf("\nfarenheit to celsius conversion\n_________________________________\n\n");
+  printf("\nfarhenheit to celsius conversion\n_________________________________\n\n");
   float fahr, celsius;
   float lower, step, upper;
   lower = 0;
@@ -10,9 +10,16 @@ int main()
   step = 20;
   fahr = lower;
   while (fahr <= upper) {
-    celsius = (5.0/9.0) * (fahr-32.0);
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
     fahr = fahr + step;
+  }
+  printf("_________________________________\n\ncelsius to farhenheit conversion\n_________________________________\n\n");
+  celsius = lower;
+  while (celsius <= upper) {
+    fahr = ((9.0 / 5.0) * celsius) + 32;
+    printf("%3.0f\t%6.1f\n", celsius, fahr);
+    celsius = celsius + step;
   }
   return 0;
 }
