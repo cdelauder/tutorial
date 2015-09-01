ffce4d82988e1d97f624d1b26fe19199bc79493d
cdelauder <cdelauder@gmail.com>
2015-08-31 20:42:57 -0700

    now using #define

--- hello.c
+++ hello.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
 
 int main()
 {
   printf("\nfarhenheit to celsius conversion\n_________________________________\n\n");
   float fahr, celsius;
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     printf("%3.0f\t%6.1f\n", fahr, (fahr + 32) * (5.0 / 9.0));
   printf("_________________________________\n\ncelsius to farhenheit conversion\n_________________________________\n\n");
-  for (celsius = 300; celsius >= 0; celsius = celsius - 20)
+  for (celsius = UPPER; celsius >= LOWER; celsius = celsius - STEP)
     printf("%3.0f\t%6.1f\n", celsius, (celsius * (9.0 / 5.0)) - 32);
   return 0;
 }
