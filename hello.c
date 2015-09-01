7819ef17b715c6d4acd9a1f78106cdb792442eb7
cdelauder <cdelauder@gmail.com>
2015-08-31 20:46:10 -0700

    very simple collect user input and display program

--- hello.c
+++ hello.c
@@ -5,13 +5,12 @@
 
 int main()
 {
-  printf("\nfarhenheit to celsius conversion\n_________________________________\n\n");
-  float fahr, celsius;
-  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
-    printf("%3.0f\t%6.1f\n", fahr, (fahr + 32) * (5.0 / 9.0));
-  printf("_________________________________\n\ncelsius to farhenheit conversion\n_________________________________\n\n");
-  for (celsius = UPPER; celsius >= LOWER; celsius = celsius - STEP)
-    printf("%3.0f\t%6.1f\n", celsius, (celsius * (9.0 / 5.0)) - 32);
+  int c;
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
   return 0;
 }
 
